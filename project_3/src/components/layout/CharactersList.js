50b310106d22cbd37de0d9c2c6a00642b209c2ee
Irina Mishchenko <i.mishchenko95@gmail.com>
2023-06-19 18:37:57 +0300

    added search to CharactersList and ComicsList

--- project_3/src/components/layout/CharactersList.js
+++ project_3/src/components/layout/CharactersList.js
@@ -8,24 +8,47 @@ const API_key = "95857d6d985fa57f979a3eca57531d54";
 function CharactersList() {
   const [characters, setCharacters] = useState(null);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState(null);
 
-  async function fetchCharacters() {
-    axios
-      .get(baseURL, {
-        params: {
-          apikey: API_key,
-          events: 310,
-          limit: 42,
-          offset: 1,
-        },
-      })
-      .then((response) => setCharacters(response.data.data.results))
-      .catch((error) => setError(error.message));
+  async function fetchCharacters(search) {
+    if (!search) {
+      axios
+        .get(baseURL, {
+          params: {
+            apikey: API_key,
+            events: 310,
+            limit: 42,
+            offset: 1,
+          },
+        })
+        .then((response) => setCharacters(response.data.data.results))
+        .catch((error) => setError(error.message));
+    } else if (search) {
+      axios
+        .get(baseURL, {
+          params: {
+            apikey: API_key,
+            limit: 42,
+            nameStartsWith: search,
+          },
+        })
+        .then((response) => setCharacters(response.data.data.results))
+        .catch((error) => setError(error.message));
+    }
   }
 
   useEffect(() => {
     fetchCharacters();
   }, []);
+
+  function handleChange(e) {
+    fetchCharacters(search);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchCharacters(search);
+  }
 
   if (error) {
     return (
@@ -52,7 +75,28 @@ function CharactersList() {
         <h2 className="characters-item-name">{character.name}</h2>
       </li>
     ));
-    return <ul className="characters-list">{charactersItems}</ul>;
+    return (
+      <>
+        <form
+          className="characters-form"
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="characters-input"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <input
+            className="characters-form-button"
+            type="submit"
+            value="Search"
+          />
+        </form>
+        <ul className="characters-list">{charactersItems}</ul>
+      </>
+    );
   }
 }
 
--- project_3/src/components/layout/ComicsList.js
+++ project_3/src/components/layout/ComicsList.js
@@ -8,24 +8,54 @@ const API_key = "95857d6d985fa57f979a3eca57531d54";
 function ComicsList() {
   const [comics, setComics] = useState(null);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState(null);
+  const [type, setType] = useState(null);
+  const [year, setYear] = useState(null);
+  const [format, setFormat] = useState(null);
 
-  async function fetchComics() {
-    axios
-      .get(baseURL, {
-        params: {
-          apikey: API_key,
-          format: "hardcover",
-          limit: 21,
-          offset: 3,
-        },
-      })
-      .then((response) => setComics(response.data.data.results))
-      .catch((error) => setError(error.message));
+  const formats = ["comic", "magazine", "hardcover", "digest"];
+
+  async function fetchComics(search, type, year, format) {
+    if (!search && !type && !year && !format) {
+      axios
+        .get(baseURL, {
+          params: {
+            apikey: API_key,
+            format: "hardcover",
+            limit: 21,
+            offset: 3,
+          },
+        })
+        .then((response) => setComics(response.data.data.results))
+        .catch((error) => setError(error.message));
+    } else if (search || type || year || format) {
+      axios
+        .get(baseURL, {
+          params: {
+            apikey: API_key,
+            titleStartsWith: search,
+            formatType: type,
+            startYear: year,
+            format: format,
+          },
+        })
+        .then((response) => setComics(response.data.data.results))
+        .catch((error) => setError(error.message));
+    }
   }
 
   useEffect(() => {
     fetchComics();
   }, []);
+
+  function handleChange() {
+    fetchComics(search, type);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchComics(search, type, year, format);
+  }
 
   if (error) {
     return (
@@ -49,7 +79,70 @@ function ComicsList() {
         <h2 className="comics-item-title">{item.title}</h2>
       </li>
     ));
-    return <ul className="comics-list">{comicsItems}</ul>;
+    const formatItems = formats.map((format) => (
+      <option key={format}>{format}</option>
+    ));
+    return (
+      <>
+        <form
+          className="comics-form"
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="comics-title-input"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div className="comics-type-wrapper">
+            <h3>Type:</h3>
+            <label className="comics-type-title">
+              <input
+                className="comics-type-input"
+                type="radio"
+                id="comic"
+                name="type"
+                value="comic"
+                onChange={(e) => setType(e.target.value)}
+              />
+              comic
+            </label>
+            <label className="comics-type-title">
+              <input
+                className="comics-type-input"
+                type="radio"
+                id="collection"
+                name="type"
+                value="collection"
+                onChange={(e) => setType(e.target.value)}
+              />
+              collection
+            </label>
+          </div>
+          <label className="comics-year-title">
+            Start year:{" "}
+            <input
+              className="comics-year-input"
+              type="text"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+          </label>
+          <label className="comics-format-title">
+            Format:
+            <select
+              className="comics-format-select"
+              onChange={(e) => setFormat(e.target.value)}
+            >
+              {formatItems}
+            </select>
+          </label>
+          <input className="comics-form-button" type="submit" value="Search" />
+        </form>
+        <ul className="comics-list">{comicsItems}</ul>
+      </>
+    );
   }
 }
 
